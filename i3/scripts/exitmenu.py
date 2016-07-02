06fb0a2a81d1abba0b2fb0966dc33d48328cc589
Aaron Williamson <guitarfanman@gmail.com>
2016-07-01 22:16:06 -0600

    Updates

--- i3/scripts/exitmenu.py
+++ i3/scripts/exitmenu.py
@@ -10,107 +10,111 @@ logoutCmd = ["/usr/bin/i3-msg", "exit"]
 shutdownCmd = ["sudo", "systemctl", "poweroff"]
 rebootCmd = ["sudo", "systemctl", "reboot"]
 
-class MyWindow(Gtk.Window):
+class ExitMenu(Gtk.Window):
 
-	# Constructor
-	def __init__(self):
-		# Customizable exit dialog
-		exitDialog = "Exiting i3wm..."
+    # Constructor
+    def __init__(self):
+        # Customizable exit dialog
+        exitDialog = "Exiting i3wm..."
 
-		Gtk.Window.__init__(self, title="Exit")
-		self.set_border_width(10)
-		self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
-		self.set_decorated(False)
+        Gtk.Window.__init__(self, title="Exit")
 
-		# Enable button images
-		settings = Gtk.Settings.get_default()
-		settings.set_property("gtk_button_images", True)
+        self.set_border_width(10)
+        self.set_type_hint(Gdk.WindowTypeHint.DIALOG)
+        self.set_decorated(False)
 
-		# The dialog for the exit menu
-		exitDialog = Gtk.Label(exitDialog)
+        # Enable button images
+        settings = Gtk.Settings.get_default()
+        settings.set_property("gtk_button_images", True)
 
-		# Create the different boxes
-		vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
-		hbox1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
-		hbox2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
-		hbox3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
-		dummybox = Gtk.Box()
+        # The dialog for the exit menu
+        exitDialog = Gtk.Label(exitDialog)
 
-		# Set homogeneity
-		vbox.set_homogeneous(True)
-		hbox1.set_homogeneous(True)
-		hbox2.set_homogeneous(True)
-		hbox3.set_homogeneous(True)
+        # Create the different boxes
+        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=10)
+        hbox1 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        hbox2 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        hbox3 = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=6)
+        dummybox = Gtk.Box()
 
-		# Add the master box to our panel
-		self.add(vbox)
+        # Set homogeneity
+        vbox.set_homogeneous(True)
+        hbox1.set_homogeneous(True)
+        hbox2.set_homogeneous(True)
+        hbox3.set_homogeneous(True)
 
-		# Add the dialog and both sub-boxes
-		vbox.pack_start(exitDialog, True, True, 0)
-		vbox.pack_start(hbox1, True, True, 0)
-		vbox.pack_start(hbox2, True, True, 0)
-		vbox.pack_start(hbox3, True, True, 0)
+        # Add the master box to our panel
+        self.add(vbox)
 
-		# Get all the images needed
-		shutdownIcon = Gtk.Image()
-		shutdownIcon.set_from_icon_name("system-shutdown", 2)
+        # Add the dialog and both sub-boxes
+        vbox.pack_start(exitDialog, True, True, 0)
+        vbox.pack_start(hbox1, True, True, 0)
+        vbox.pack_start(hbox2, True, True, 0)
+        vbox.pack_start(hbox3, True, True, 0)
 
-		rebootIcon = Gtk.Image()
-		rebootIcon.set_from_icon_name("system-reboot", 2)
+        # Get all the images needed
+        shutdownIcon = Gtk.Image()
+        shutdownIcon.set_from_icon_name("system-shutdown", 2)
 
-		logoutIcon = Gtk.Image()
-		logoutIcon.set_from_icon_name("system-log-out", 2)
-		logoutIcon.set_padding(2,0)
+        rebootIcon = Gtk.Image()
+        rebootIcon.set_from_icon_name("system-reboot", 2)
 
-		lockIcon = Gtk.Image()
-		lockIcon.set_from_icon_name("system-lock-screen", 2)
-		lockIcon.set_padding(2,0)
+        logoutIcon = Gtk.Image()
+        logoutIcon.set_from_icon_name("system-log-out", 2)
+        logoutIcon.set_padding(2,0)
 
-		cancelIcon = Gtk.Image()
-		cancelIcon.set_from_icon_name("edit-delete", 1)
+        lockIcon = Gtk.Image()
+        lockIcon.set_from_icon_name("system-lock-screen", 2)
+        lockIcon.set_padding(2,0)
 
-		self.shutdownButton = Gtk.Button(label="Shut Down")
-		self.shutdownButton.set_image(shutdownIcon)
-		self.shutdownButton.connect("clicked", self.shutdown)
-		hbox1.pack_start(self.shutdownButton, True, True, 0)
+        cancelIcon = Gtk.Image()
+        cancelIcon.set_from_icon_name("edit-delete", 1)
 
-		self.rebootButton = Gtk.Button(label="Reboot")
-		self.rebootButton.set_image(rebootIcon)
-		self.rebootButton.connect("clicked", self.reboot)
-		hbox1.pack_start(self.rebootButton, True, True, 0)
+        shutdownButton = Gtk.Button(label="Shut Down")
+        shutdownButton.set_image(shutdownIcon)
+        shutdownButton.connect("clicked", self.shutdown)
+        hbox1.pack_start(shutdownButton, True, True, 0)
 
-		self.lockButton = Gtk.Button(label="Lock")
-		self.lockButton.set_image(lockIcon)
-		self.lockButton.connect("clicked", self.lock)
-		hbox2.pack_start(self.lockButton, True, True, 0)
+        rebootButton = Gtk.Button(label="Reboot")
+        rebootButton.set_image(rebootIcon)
+        rebootButton.connect("clicked", self.reboot)
+        hbox1.pack_start(rebootButton, True, True, 0)
 
-		self.logoutButton = Gtk.Button(label="Log Out")
-		self.logoutButton.set_image(logoutIcon)
-		self.logoutButton.connect("clicked", self.logout)
-		hbox2.pack_start(self.logoutButton, True, True, 0)
+        lockButton = Gtk.Button(label="Lock")
+        lockButton.set_image(lockIcon)
+        lockButton.connect("clicked", self.lock)
+        hbox2.pack_start(lockButton, True, True, 0)
 
-		self.cancelButton = Gtk.Button(label="Cancel")
-		self.cancelButton.set_image(cancelIcon)
-		self.cancelButton.connect("clicked", Gtk.main_quit)
-		hbox3.pack_start(dummybox, True, True, 0)
-		hbox3.pack_start(self.cancelButton, True, True, 0)
+        logoutButton = Gtk.Button(label="Log Out")
+        logoutButton.set_image(logoutIcon)
+        logoutButton.connect("clicked", self.logout)
+        hbox2.pack_start(logoutButton, True, True, 0)
 
-	# Function definitions
-	def logout(self, widget):
-		subprocess.call(logoutCmd)
-		Gtk.main_quit()
+        cancelButton = Gtk.Button(label="Cancel")
+        cancelButton.set_image(cancelIcon)
+        cancelButton.connect("clicked", Gtk.main_quit)
+        hbox3.pack_start(dummybox, True, True, 0)
+        hbox3.pack_start(cancelButton, True, True, 0)
 
-	def shutdown(self, widget):
-		subprocess.call(shutdownCmd)
+    # Function definitions
+    def logout(self, widget):
+        subprocess.call(logoutCmd)
+        Gtk.main_quit()
 
-	def reboot(self, widget):
-		subprocess.call(rebootCmd)
+    def shutdown(self, widget):
+        subprocess.call(logoutCmd)
+        subprocess.call(shutdownCmd)
 
-	def lock(self, widget):
-		subprocess.call(lockProg)
-		Gtk.main_quit()
+    def reboot(self, widget):
+        subprocess.call(logoutCmd)
+        subprocess.call(rebootCmd)
 
-win = MyWindow()
+    def lock(self, widget):
+        subprocess.call(lockProg)
+        Gtk.main_quit()
+
+# Main function
+win = ExitMenu()
 win.connect("delete-event", Gtk.main_quit)
 win.show_all()
 Gtk.main()
